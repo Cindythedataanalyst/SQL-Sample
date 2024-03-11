45781b7fd121d07296a2f622e092ebcec8b5a1c4
Cindy Jiang <162639642+Cindythedataanalyst@users.noreply.github.com>
2024-03-11 11:28:58 +1100

    Update divvy_tripdata.sql

--- divvy_tripdata.sql
+++ divvy_tripdata.sql
@@ -1,83 +1,72 @@
---Background:Investigating the dynamics of bike-sharing usage through data analysis, our study aims to uncover how 
---different user groups interact with the Cyclistic bike-share system. 
+/**Background:
+This Capstone Project marks the culmination of completing the Google Data Analytics Professional Certificate. As a junior data analyst at 
+Cyclistic, my task involves analyzing historical bike trip data to inform marketing strategies, specifically targeting the conversion of 
+casual riders to annual members. Working alongside the marketing director, our objective is to understand the behaviors and preferences
+of these rider segments through thorough data analysis and visualization, paving the way for targeted marketing campaigns and strategic
+decision-making.**/
+
+--Step 1: Ask
 
 --Target question: How do annual members and casual riders use Cyclistic bikes differently?
---Data cleaning partly done in Excel before import the data.
---Import the last 12 months(202302-202401) bike share history.
 
---Preview data.
+--Step 2: Prepare
 
-Select *
-From dbo.[202401-divvy-tripdata];
+/**Target question: How do annual members and casual riders use Cyclistic bikes differently?
+The analysis relies on Cyclistic's historical trip data, covering the last six months, sourced from Motivate International Inc. under a 
+specific license. **/
 
-Select *
-From dbo.[202302-divvy-tripdata];
+/**Data cleaning was partially completed in Excel before importing the data. This process involved making copy from raw 
+data for data cleaning, removing irrelevant and duplicate data, fixing structural errors, and handling missing data.**/
 
---Combine all the data into one table by using CTE and union all function.
+--Import the Cyclistic's historical trip data of the previous 6 months.
+--Combining the last 6 months Cyclistic's historical trip data.
 
-WITH joined_data AS (
-SELECT *, '2303' AS source_table FROM Casestudy.dbo.[202303-divvy-tripdata]
-UNION ALL
-SELECT *, '2304' AS source_table FROM Casestudy.dbo.[202304-divvy-tripdata]
-UNION ALL
-SELECT *, '2305' AS source_table FROM Casestudy.dbo.[202305-divvy-tripdata]
-UNION ALL
-SELECT *, '2306' AS source_table FROM Casestudy.dbo.[202306-divvy-tripdata]
-UNION ALL
-SELECT *, '2307' AS source_table FROM Casestudy.dbo.[202307-divvy-tripdata]
-UNION ALL
-SELECT *, '2308' AS source_table FROM Casestudy.dbo.[202308-divvy-tripdata]
-UNION ALL
-SELECT *, '2310' AS source_table FROM Casestudy.dbo.[202310-divvy-tripdata]
-UNION ALL
-SELECT *, '2311' AS source_table FROM Casestudy.dbo.[202311-divvy-tripdata]
-UNION ALL
-SELECT *, '2312' AS source_table FROM Casestudy.dbo.[202312-divvy-tripdata]
-UNION ALL
-SELECT *, '2302' AS source_table FROM Casestudy.dbo.[202302-divvy-tripdata]
-UNION ALL
-SELECT *, '2309' AS source_table FROM Casestudy.dbo.[202309-divvy-tripdata]
-UNION ALL
-SELECT *, '2401' AS source_table FROM Casestudy.dbo.[202401-divvy-tripdata]
-)
-SELECT * INTO divvy_tripdata FROM joined_data;
-
---Preview divvy_tripdata.
+SELECT * INTO joined_data
+FROM (
+    SELECT * FROM [Casestudy].[dbo].[202312-divvy-tripdata]
+    UNION ALL
+    SELECT * FROM [Casestudy].[dbo].[202311-divvy-tripdata]
+	union all
+	 SELECT * FROM [Casestudy].[dbo].[202401-divvy-tripdata]
+    UNION ALL
+    SELECT * FROM [Casestudy].[dbo].[202310-divvy-tripdata]
+	  UNION ALL
+    SELECT * FROM [Casestudy].[dbo].[202309-divvy-tripdata]
+	  UNION ALL
+    SELECT * FROM [Casestudy].[dbo].[202308-divvy-tripdata]
+) AS combined_data;
 
 Select *
-From divvy_tripdata;
+From [Casestudy].[dbo].[joined_data];
 
---Start analyzing the data.
+--Continue data clean in SQL.
 
---Preview data type before calculation.
+--Data cleaning: convert data types.Convert ride_length_mn data to decimal for future calculation.
 
-EXEC sp_help 'dbo.divvy_tripdata';
+ALTER TABLE [Casestudy].[dbo].[joined_data]
+ALTER COLUMN ride_length_mn DECIMAL(10, 2);
 
---Convert data type before calculation.
---Convert day_of_week int, ride length minutes decimal.
+/**Data cleaning: standardize and validate data. the combined data covered the bike share history in the last 12 months,
+the data followed the constraints for its feild.the data makes sense for analysis. **/
 
-Alter table dbo.divvy_tripdata
-Alter Column day_of_week int;
 
-Select CAST(ride_length_minutes AS decimal(10,2)) As ride_length
-From divvy_tripdata;
-
-Alter table dbo.divvy_tripdata
-Add ride_length decimal(10,2);
-
-Update dbo.divvy_tripdata
-Set ride_length = CAST(ride_length_minutes AS decimal(10,2));
+Select 
+	sum(Cast(ride_length_mn as decimal(10,2))) as total_ride_length_mn,
+	member_casual
+FROM [Casestudy].[dbo].[202302-divvy-tripdata]
+Group By
+	member_casual;
 
 --Select data we are going to be using.
 
---Looking at the frequency of rides for each group,
---to see if one group tends to use the bikes more frequently than the other.
+/**Looking at the frequency of rides for each group, to see if one group tends to use the bikes more frequently than 
+the other, which showed members use the bike more frequently.**/
 
 SELECT 
     member_casual,
     COUNT(*) AS ride_count
 FROM 
-    [Casestudy].[dbo].[divvy_tripdata]
+    [Casestudy].[dbo].[joined_data]
 GROUP BY 
     member_casual;
 
@@ -86,9 +75,9 @@ GROUP BY
 SELECT 
     member_casual,
     COUNT(*) AS total_rides,
-    AVG(ride_length)
-	AS average_ride_length_minutes
-FROM dbo.divvy_tripdata
+    AVG(ride_length_mn)
+	AS average_ride_duration
+FROM [Casestudy].[dbo].[joined_data]
 GROUP BY 
     member_casual;
 
@@ -97,7 +86,7 @@ GROUP BY
 Select 
 	member_casual,
 	COUNT (member_casual) AS member_casual_count
-From dbo.divvy_tripdata
+From [Casestudy].[dbo].[joined_data]
 Group by
 	member_casual;
 
@@ -105,28 +94,30 @@ SELECT
     member_casual,
     COUNT(*) AS member_casual_count,
     (COUNT(*) * 100.0 / SUM(COUNT(*)) OVER ()) AS percentage
-FROM dbo.divvy_tripdata
+FROM [Casestudy].[dbo].[joined_data]
 GROUP BY member_casual;
 
 --Analyzing the average ride length on different days of the week for both member and casual riders.
 
 Select 
+	top 3
 	member_casual,
 	COUNT (member_casual) AS member_casual_count,
 	day_of_week
-From [Casestudy].[dbo].[divvy_tripdata]
+From [Casestudy].[dbo].[joined_data]
 Group by
 	member_casual,
 	day_of_week
-Order by day_of_week;
+Order by 
+	day_of_week;
 
 --Analyzing the longest ride length, and the shortest ride length.
 
 Select 
 	member_casual,
-	max (ride_length) AS longest_ride_length,
-	min (ride_length) as shortest_ride_length
-From [Casestudy].[dbo].[divvy_tripdata]
+	max (ride_length_mn) AS longest_ride_length,
+	min (ride_length_mn) as shortest_ride_length
+From [Casestudy].[dbo].[joined_data]
 Group by
 	member_casual;
 
@@ -136,118 +127,63 @@ Select
 	member_casual,
 	rideable_type,
 	Count (*) As ride_count
-From [Casestudy].[dbo].[divvy_tripdata]
+From [Casestudy].[dbo].[joined_data]
+Where rideable_type <>'docked_bike'
 Group by
 	member_casual,
 	rideable_type;
-
---Trying to get rid of repeated ride type rows.
-
-SELECT 
-    member_casual,
-    SUM(CASE WHEN rideable_type = 'classic_bike' THEN 1 ELSE 0 END) AS bike_count,
-    SUM(CASE WHEN rideable_type = 'electric_bike' THEN 1 ELSE 0 END) AS ebike_count,
-    SUM(CASE WHEN rideable_type = 'docked_bike' THEN 1 ELSE 0 END) AS docked_bike_count
-FROM 
-    [Casestudy].[dbo].[divvy_tripdata]
-GROUP BY 
-    member_casual,
-	rideable_type;
-
-SELECT 
-    member_casual,
-    SUM(CASE WHEN rideable_type = 'classic_bike' THEN 1 ELSE 0 END) AS bike_count,
-    SUM(CASE WHEN rideable_type = 'electric_bike' THEN 1 ELSE 0 END) AS ebike_count,
-    SUM(CASE WHEN rideable_type = 'docked_bike' THEN 1 ELSE 0 END) AS docked_bike_count
-FROM 
-    [Casestudy].[dbo].[divvy_tripdata]
-GROUP BY 
-    member_casual
-HAVING 
-    SUM(CASE WHEN rideable_type = 'classic_bike' THEN 1 ELSE 0 END) > 0
-    OR SUM(CASE WHEN rideable_type = 'electric_bike' THEN 1 ELSE 0 END) > 0
-    OR SUM(CASE WHEN rideable_type = 'docked_bike' THEN 1 ELSE 0 END) > 0;
-
---Add ride year month to calculate the difference in months.
-
-Alter table [Casestudy].[dbo].[divvy_tripdata]
-Add ride_year varchar (4);
-
-UPDATE [Casestudy].[dbo].[divvy_tripdata]
-SET ride_year = SUBSTRING(CONVERT(VARCHAR, ride_date, 120), 1, 4);
-
-Select top 100 *
-From [Casestudy].[dbo].[divvy_tripdata];
-
---Delete unused columns.
-
-Alter table [Casestudy].[dbo].[divvy_tripdata]
-Drop Column source_table,
-Drop Column ride_date;
 
 --Analyzing the longest and shortest ride lengths of the month.
 
 Select 
 	member_casual,
-	ride_year,
-	max (ride_length) AS longest_ride_length,
-	min (ride_length) as shortest_ride_length
-From [Casestudy].[dbo].[divvy_tripdata]
+	ride_date,
+	max (ride_length_mn) AS longest_ride_length,
+	min (ride_length_mn) as shortest_ride_length
+From [Casestudy].[dbo].[joined_data]
 Group by
 	member_casual,
-	ride_year
+	ride_date
 Order by
-	ride_year;
+	ride_date;
 
 --Analyzing the total ride length by month for both member and casual riders.
 
 Select 
 	member_casual,
-	ride_year,
-	SUM (ride_length) AS total_ride_length
-From [Casestudy].[dbo].[divvy_tripdata]
+	ride_date,
+	SUM (ride_length_mn) AS total_ride_length
+From [Casestudy].[dbo].[joined_data]
 Group by
 	member_casual,
-	ride_year
+	ride_date
 Order by
-	ride_year;
+	ride_date;
 
 --Analyzing the difference in average ride length between members and casual riders across different months.
 
 Select 
 	member_casual,
-	ride_year,
-	avg (ride_length) AS average_ride_length
-From [Casestudy].[dbo].[divvy_tripdata]
+	ride_date,
+	avg (ride_length_mn) AS average_ride_length
+From [Casestudy].[dbo].[joined_data]
 Group by
 	member_casual,
-	ride_year
+	ride_date
 Order by
-	ride_year;
+	ride_date;
 
 --Displaying the top 3 months with the longest average ride length for both members and casual riders.
 
-ALTER TABLE Casestudy.dbo.divvy_tripdata
-ADD ride_year_month varchar (4);
-
-UPDATE Casestudy.dbo.divvy_tripdata
-SET ride_year_month = ride_year;
-
-Select top 100 *
-From [Casestudy].[dbo].[divvy_tripdata];
-
-ALTER TABLE Casestudy.dbo.divvy_tripdata
-DROP COLUMN ride_year;
-
 SELECT 
---TOP 3
-    member_casual,
-    ride_year_month,
-    AVG(ride_length) AS average_ride_length
-FROM [Casestudy].[dbo].[divvy_tripdata]
+    TOP 3
+    ride_date,
+	member_casual,
+    AVG(ride_length_mn) AS average_ride_length
+FROM [Casestudy].[dbo].[joined_data]
 GROUP BY
-    member_casual,
-    ride_year_month
+	member_casual,
+    ride_date
 ORDER BY
     average_ride_length DESC;
 
@@ -255,10 +191,23 @@ ORDER BY
 
 Select 
 	day_of_week,
-	member_casual,
-	avg(ride_length) as average_ride_length
+	avg(ride_length_mn) as average_ride_length
 FROM 
-	[Casestudy].[dbo].[divvy_tripdata]
+	[Casestudy].[dbo].[joined_data]
+Group By
+	day_of_week
+Order by
+	average_ride_length desc;
+
+Select 
+	day_of_week,
+	member_casual,
+	avg(ride_length_mn) as average_ride_length,
+	Count(*) as ride_count
+FROM 
+	[Casestudy].[dbo].[joined_data]
+Where 
+	day_of_week IN (1,7)
 Group By
 	day_of_week,
 	member_casual
@@ -279,10 +228,17 @@ SELECT
     END AS day_of_week_name,
     COUNT(*) AS total_rides
 FROM
-    [Casestudy].[dbo].[divvy_tripdata]
+    [Casestudy].[dbo].[joined_data]
 GROUP BY
     CASE
         WHEN day_of_week IN (1, 7) THEN 'Weekend'
         ELSE 'Weekday'
     END,
-    day_of_week; 
+    day_of_week
+Order by
+	total_rides desc; 
+
+/**Through meticulous SQL analysis amalgamating six datasets, I delineated the divergent riding behaviors of Cyclistic's members and casual
+riders, revealing members' higher ride frequency and casual riders' penchant for longer journeys. Surprisingly, both groups converge on 
+Wednesdays as their peak riding day, with August 2023 standing out as the pinnacle of ridership, laying a compelling foundation for 
+strategic marketing initiatives.**/
